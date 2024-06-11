de0308a564eec2dc2993f260b9af2bec2f89e43f
@oleggabanov <you@example.com>
2024-06-11 19:16:30 +0300

    first day commit

--- src/Day1.java
+++ src/Day1.java
@@ -3,6 +3,7 @@ import java.util.HashSet;
 public class Day1 {
   public static void main(String[] args) {
     System.out.println(hasDuplicate(new int[] { 1, 1, 3, 4, 5, 6, 7, 8, 9, 10 }));
+    System.out.println("SIUUUUU");
   }
   public static boolean hasDuplicate(int[] nums) {
     HashSet<Integer> set = new HashSet<>();
