d85bd517881c29fea79fc528a085137a2e349f02
@oleggabanov <you@example.com>
2024-10-09 13:33:08 +0300

    bubble sort

--- src/Day1.java
+++ src/Day1.java
@@ -2,17 +2,19 @@ import java.util.HashSet;
 
 public class Day1 {
   public static void main(String[] args) {
-    System.out.println(hasDuplicate(new int[] { 1, 1, 3, 4, 5, 6, 7, 8, 9, 10 }));
-    System.out.println("SIUUUUU");
-  }
-  public static boolean hasDuplicate(int[] nums) {
-    HashSet<Integer> set = new HashSet<>();
-    for (int i = 0; i < nums.length; i++) {
-      if (set.contains(nums[i])) {
-        return true;
+    int[] array2 = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9};
+    boolean isSorted = false;
+    int tempValue2 = 52;
+    while (!isSorted) {
+      isSorted = true;
+      for (int i = 1; i < array2.length; i++) {
+        if (array2[i] < array2[i - 1]) {
+          tempValue2 = array2[i];
+          array2[i] = array2[i - 1];
+          array2[i - 1] = tempValue2;
+          isSorted = false;
+        }
       }
-      set.add(nums[i]);
     }
-    return false;
   }
 }
